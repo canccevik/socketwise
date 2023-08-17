197ce68b7aa0eb29585ee9026f67defdc83eabfe
Can Çevik <mazlumcancevik@gmail.com>
2023-08-18 02:40:08 +0300

    feat: add get classes by suffix util

---
+++ src/utils/get-classes-by-suffix.util.ts
@@ -0,0 +1,19 @@
+import fg from 'fast-glob'
+import { Type } from '../interfaces'
+
+export async function getClassesBySuffix(suffix: string): Promise<Type[]> {
+  const classes: Type[] = []
+  const files = fg.sync(`**/${suffix}`, { absolute: true })
+
+  for await (const file of files) {
+    try {
+      const exports = await import(file)
+      Object.values(exports).forEach((exportedClass: any) => {
+        exportedClass.constructor && classes.push(exportedClass)
+      })
+    } catch (error) {
+      throw new Error(`Cannot read files with matched ${suffix} suffix.`)
+    }
+  }
+  return classes
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export * from './get-classes-by-suffix.util'
