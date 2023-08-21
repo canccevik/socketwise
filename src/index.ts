c2383f1fef0499bcc019d0a5287420f562e18140
Can Çevik <mazlumcancevik@gmail.com>
2023-08-21 17:37:30 +0300

    feat: export magnodi decorators

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import 'reflect-metadata'
 export * from './socketwise'
 export * from './decorators'
 
+export { Inject, Injectable } from 'magnodi'
 export { SocketwiseMiddleware, SocketIONextFunc } from './interfaces'
